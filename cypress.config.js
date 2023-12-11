9c20639510a92b343310831c11c7886813b5ff0c
Jovana Ranđelović <jovana.r2011@gmail.com>
2023-12-11 10:52:53 +0100

    add viewport and timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 20000,
+  watchForFileChanges: true,
+  numTestKeptInMemory: 1,
+  requestTimeout: 15000,
+  responseTimeout: 15000,
+  viewportWidth: 768,
+  viewportHeight: 1000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://constel-social-network.vercel.app/login",
   },
 });
